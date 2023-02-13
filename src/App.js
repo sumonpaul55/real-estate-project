528639d9cbf226279c829daae795f41bb0790e29
sumon paul <sumpaul100@gmail.com>
2023-02-13 23:13:38 +0600

    real-estate project start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+// import routes and route
+import { Routes, Route } from "react-router-dom";
+// import components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import PropertyDetails from "./pages/PropertyDetails";
 
 const App = () => {
-  return <div>react app</div>;
+  return (
+    <div className="text-center">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/property/:id" element={<PropertyDetails />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
